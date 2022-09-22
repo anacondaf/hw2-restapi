00fb935362815240bf230ac4479d499d9e3c17b1
KaiNguyen <kainguyen.business@gmail.com>
2022-09-22 13:20:33 +0700

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+//API's operations: CRUD (Create, Read, Update, Delete)
+//HTTP METHOD: GET, POST, PUT, PATCH, DELETE
+
+//GET endpoint: /api/getStudent
+app.get("/api/getStudent", (req, res) => {
+	const students = ["Khang", "Khai", "Minh"];
+
+	res.json(students);
+});
+
+
+
+//PORT: 3000, 3001, 3002, 3003, 5001, 8080, 8081
+app.listen(3000, () => {
+	console.log("Running");
+});
